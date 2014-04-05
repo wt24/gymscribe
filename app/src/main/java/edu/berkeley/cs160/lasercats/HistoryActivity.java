bfe60f7e8296fa90ada849d8171e9e00da1e9bd3
Steven Pham <vanp1992@yahoo.com>
2014-04-05 15:42:04 -0700

    Changed Past to History

--- app/src/main/java/edu/berkeley/cs160/lasercats/HistoryActivity.java
+++ app/src/main/java/edu/berkeley/cs160/lasercats/HistoryActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class PastActivity extends BaseNavigationDrawerActivity {
+public class HistoryActivity extends BaseNavigationDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
