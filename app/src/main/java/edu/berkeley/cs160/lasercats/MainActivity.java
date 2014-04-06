eaa4412c131b88554f8481aa3c1d6fa42f0ebe94
Opal Kale <opal.kale@gmail.com>
2014-04-06 14:52:19 -0700

    first edit of edit

--- app/src/main/java/edu/berkeley/cs160/lasercats/MainActivity.java
+++ app/src/main/java/edu/berkeley/cs160/lasercats/MainActivity.java
@@ -5,19 +5,74 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends ActionBarActivity {
+    private Button mEditButton;
+    private ListView mListView;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String[] exercises = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        mListView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, R.layout.row, exercises);
+
+        mListView.setAdapter(adapter);
+
+
+
+        //Start: Edit Button
+        mEditButton = (Button) findViewById(R.id.editButton);
+        mEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do shit
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+        //End: Edit Button
+
         startActivity(new Intent(this, RecordActivity.class));
 
     }
